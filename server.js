e9567098245c1b0ab35c67857a68c237adef5eb5
Mushe <mosescolince161@gmail.com>
2024-10-31 16:06:59 +0200

    Node yah

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import http from 'http';
-const PORT = 5000
+const PORT = process.env.PORT
 
 const server = http.createServer((req, res) => {
-    res.setHeader('content-type', 'text/html');
-    res.statusCode = 404 ;
+    if (req.url === '/' ) {
+        res.writeHead(200, {'Content-Type': 'text-html'})
+        res.end('<h1>Homepage </h1>');
+    } else if (req.url === '/about'){
+        res.writeHead(200, {'Content-Type': 'text-html'})
+        res.end('<h1>About </h1>');
+    } else {
+        res.writeHead(404, {'Content-Type': 'text-html'})
+        res.end('<h1>Not Found </h1>');
+    }
 
-    res.end('<h1>Hello World </h1>');
+
 });
+
+
 
 server.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
